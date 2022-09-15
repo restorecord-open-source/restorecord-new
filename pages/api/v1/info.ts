3ba1cc0bb667d9a34e94b03719365bb5d24b1906
xenos#1337 <66328734+xenos1337@users.noreply.github.com>
2022-09-15 17:44:57 +0200

    info

--- pages/api/v1/info.ts
+++ pages/api/v1/info.ts
@@ -16,20 +16,29 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     let last24hCount = Array.from({ length: 24 }, (_, i) => i).map((i: any) => {
         const date = new Date();
-        date.setHours(date.getHours() - (24 - i));
-        return last24h.filter((member: any) => { return member.createdAt.getHours() === date.getHours(); }).length
+        date.setHours(date.getHours() - i);
+        return {
+            date: date,
+            count: last24h.filter((member: any) => { return member.createdAt.getHours() === date.getHours(); }).length
+        };
     });
 
     let last7dCount = Array.from({ length: 7 }, (_, i) => i).map((i: any) => {
         const date = new Date();
-        date.setDate(date.getDate() - (7 - i));
-        return last7d.filter((member: any) => { return member.createdAt.getDate() === date.getDate(); }).length
+        date.setDate(date.getDate() - i);
+        return {
+            date: date,
+            count: last7d.filter((member: any) => { return member.createdAt.getDate() === date.getDate(); }).length
+        };
     });
 
     let last30dCount = Array.from({ length: 30 }, (_, i) => i).map((i: any) => {
         const date = new Date();
-        date.setDate(date.getDate() - (30 - i));
-        return last30d.filter((member: any) => { return member.createdAt.getDate() === date.getDate(); }).length
+        date.setDate(date.getDate() - i);
+        return {
+            date: date,
+            count: last30d.filter((member: any) => { return member.createdAt.getDate() === date.getDate(); }).length
+        };
     });
 
     return res.status(200).json({
--- pages/info.tsx
+++ pages/info.tsx
@@ -35,347 +35,7 @@ async function getInfo() {
 }
 
 export default function Info() {
-    let members24h: any = [];
-    let members7d: any = [];
-    let members30d: any = [];
-
-
     const { data, isError, isLoading, refetch } = useQuery('stats', async () => await getStats(), { retry: false,  refetchOnWindowFocus: true });
-    const { data: data2, isError: isError2, isLoading: isLoading2, refetch: refetch2 } = useQuery('info', async () => await getInfo(), { retry: false,  refetchOnWindowFocus: true });
-
-    const timeArr24h = Array.from({ length: 24 }, (_, i) => {
-        const date = new Date();
-        date.setHours(date.getHours() - (24 - i));
-        return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    });
-
-    const timeArr7d = Array.from({ length: 7 }, (_, i) => {
-        const date = new Date();
-        date.setDate(date.getDate() - (7 - i));
-        return date.toLocaleDateString();
-    });
-
-    const timeArr30d = Array.from({ length: 30 }, (_, i) => {
-        const date = new Date();
-        date.setDate(date.getDate() - (30 - i));
-        return date.toLocaleDateString();
-    });
-
-    if (!isLoading2 && data2) {
-        members24h = Array.from({ length: 24 }, (_, i) => {
-            return data2.last24h.filter((x: any) => {
-                return x[i];
-            }).length;
-        });
-
-        members7d = Array.from({ length: 7 }, (_, i) => {
-            console.log(data2.last7d)
-            return data2.last7d.filter((x: any) => {
-                return x[i];
-            }).length;
-        });
-
-        members30d = Array.from({ length: 30 }, (_, i) => {
-            return data2.last30d.filter((x: any) => {
-                return x[i];
-            }).length;
-        });
-    }
-
-
-    const apexChart24: any = {
-        options: {
-            chart: {
-                id: 'members',
-                type: 'area',
-                foreColor: '#fff',
-                dropShadow: {
-                    enabled: true,
-                    top: 0,
-                    left: 0,
-                    blur: 3,
-                    opacity: 0.5
-                },
-                zoom: {
-                    enabled: false
-                },
-                toolbar: {
-                    show: false
-                },
-            },
-            dataLabels: {
-                enabled: false
-            },
-            stroke: {
-                curve: "smooth",
-                colors: ['#4f46e5'],
-            },
-            legend: {
-                horizontalAlign: 'left'
-            },
-            plotOptions: {
-                bar: {
-                    columnWidth: '30%',
-                    horizontal: false,
-                },
-            },
-            fill: {
-                colors: ['#4f46e5'],
-            },
-            tooltip: {
-                theme: 'dark',
-                marker: {
-                    show: false
-                },
-                onDatasetHover: {
-                    highlightDataSeries: false,
-                },
-            },
-            noData: {
-                text: 'No data',
-                align: 'center',
-                verticalAlign: 'middle',
-                offsetX: 0,
-                offsetY: 0,
-                style: {
-                    color: '#fff',
-                    fontSize: '14px',
-                    fontFamily: 'Roboto',
-                    fontWeight: 'bold'
-                },
-            },
-            xaxis: {
-                labels: {
-                    show: false
-                },
-                tooltip: {
-                    enabled: false
-                },
-                categories: timeArr24h
-            },
-            yaxis: {
-                show: true,
-                opposite: true,
-                labels: {
-                    offsetX: -5,
-                    formatter: function (val: any) {
-                        return val.toFixed(0);
-                    }
-                },
-            },
-            grid: {
-                show: false,
-                padding: {
-                    left: 0,
-                    right: 0,
-                    top: 0,
-                    bottom: 0
-                }
-            },
-        },
-        series: [
-            {
-                name: "Members",
-                data: members24h
-            }
-        ]
-    };
-
-    const apexChart7: any = {
-        options: {
-            chart: {
-                id: 'members',
-                type: 'area',
-                foreColor: '#fff',
-                dropShadow: {
-                    enabled: true,
-                    top: 0,
-                    left: 0,
-                    blur: 3,
-                    opacity: 0.5
-                },
-                zoom: {
-                    enabled: false
-                },
-                toolbar: {
-                    show: false
-                },
-            },
-            dataLabels: {
-                enabled: false
-            },
-            stroke: {
-                curve: "smooth",
-                colors: ['#4f46e5'],
-            },
-            legend: {
-                horizontalAlign: 'left'
-            },
-            plotOptions: {
-                bar: {
-                    columnWidth: '30%',
-                    horizontal: false,
-                },
-            },
-            fill: {
-                colors: ['#4f46e5'],
-            },
-            tooltip: {
-                theme: 'dark',
-                marker: {
-                    show: false
-                },
-                onDatasetHover: {
-                    highlightDataSeries: false,
-                },
-            },
-            noData: {
-                text: 'No data',
-                align: 'center',
-                verticalAlign: 'middle',
-                offsetX: 0,
-                offsetY: 0,
-                style: {
-                    color: '#fff',
-                    fontSize: '14px',
-                    fontFamily: 'Roboto',
-                    fontWeight: 'bold'
-                },
-            },
-            xaxis: {
-                labels: {
-                    show: false
-                },
-                tooltip: {
-                    enabled: false
-                },
-                categories: timeArr7d
-            },
-            yaxis: {
-                show: true,
-                opposite: true,
-                labels: {
-                    offsetX: -5,
-                    formatter: function (val: any) {
-                        return val.toFixed(0);
-                    }
-                },
-            },
-            grid: {
-                show: false,
-                padding: {
-                    left: 0,
-                    right: 0,
-                    top: 0,
-                    bottom: 0
-                }
-            },
-        },
-        series: [
-            {
-                name: "Members",
-                data: members7d
-            }
-        ]
-    };
-
-    const apexChart30: any = {
-        options: {
-            chart: {
-                id: 'members',
-                type: 'area',
-                foreColor: '#fff',
-                dropShadow: {
-                    enabled: true,
-                    top: 0,
-                    left: 0,
-                    blur: 3,
-                    opacity: 0.5
-                },
-                zoom: {
-                    enabled: false
-                },
-                toolbar: {
-                    show: false
-                },
-            },
-            dataLabels: {
-                enabled: false
-            },
-            stroke: {
-                curve: "smooth",
-                colors: ['#4f46e5'],
-            },
-            legend: {
-                horizontalAlign: 'left'
-            },
-            plotOptions: {
-                bar: {
-                    columnWidth: '30%',
-                    horizontal: false,
-                },
-            },
-            fill: {
-                colors: ['#4f46e5'],
-            },
-            tooltip: {
-                theme: 'dark',
-                marker: {
-                    show: false
-                },
-                onDatasetHover: {
-                    highlightDataSeries: false,
-                },
-            },
-            noData: {
-                text: 'No data',
-                align: 'center',
-                verticalAlign: 'middle',
-                offsetX: 0,
-                offsetY: 0,
-                style: {
-                    color: '#fff',
-                    fontSize: '14px',
-                    fontFamily: 'Roboto',
-                    fontWeight: 'bold'
-                },
-            },
-            xaxis: {
-                labels: {
-                    show: false
-                },
-                tooltip: {
-                    enabled: false
-                },
-                categories: timeArr30d
-            },
-            yaxis: {
-                show: true,
-                opposite: true,
-                labels: {
-                    offsetX: -5,
-                    formatter: function (val: any) {
-                        return val.toFixed(0);
-                    }
-                },
-            },
-            grid: {
-                show: false,
-                padding: {
-                    left: 0,
-                    right: 0,
-                    top: 0,
-                    bottom: 0
-                }
-            },
-        },
-        series: [
-            {
-                name: "Members",
-                data: members30d
-            }
-        ]
-    };
 
     return (
         <>
@@ -388,7 +48,7 @@ export default function Info() {
                             <Paper elevation={3}>
                                 <CardContent sx={{ alignItems: "center", justifyContent: "center", display: "flex", flexDirection: "column" }}>
                                     <ContactPageIcon sx={{ fontSize: 64, color: theme.palette.primary.main }} />
-                                    {isLoading ? <Typography variant="h5">Loading...</Typography> : <Typography variant="h5">{data.accounts}</Typography>}
+                                    {isLoading ? <Typography variant="h5">Loading...</Typography> : <Typography variant="h5">{data.accounts.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</Typography>}
                                     <Typography variant="body2" component="p" color="textSecondary">
                                         Accounts
                                     </Typography>
@@ -399,7 +59,7 @@ export default function Info() {
                             <Paper elevation={3}>
                                 <CardContent sx={{ alignItems: "center", justifyContent: "center", display: "flex", flexDirection: "column" }}>
                                     <StorageIcon sx={{ fontSize: 64, color: theme.palette.primary.main }} />
-                                    {isLoading ? <Typography variant="h5">Loading...</Typography> : <Typography variant="h5">{data.servers}</Typography>}
+                                    {isLoading ? <Typography variant="h5">Loading...</Typography> : <Typography variant="h5">{data.servers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</Typography>}
                                     <Typography variant="body2" component="p" color="textSecondary">
                                         Servers
                                     </Typography>
@@ -410,7 +70,7 @@ export default function Info() {
                             <Paper elevation={3}>
                                 <CardContent sx={{ alignItems: "center", justifyContent: "center", display: "flex", flexDirection: "column" }}>
                                     <PersonIcon sx={{ fontSize: 64, color: theme.palette.primary.main }} />
-                                    {isLoading ? <Typography variant="h5">Loading...</Typography> : <Typography variant="h5">{data.members}</Typography>}
+                                    {isLoading ? <Typography variant="h5">Loading...</Typography> : <Typography variant="h5">{data.members.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</Typography>}
                                     <Typography variant="body2" component="p" color="textSecondary">
                                         Members
                                     </Typography>
@@ -429,12 +89,6 @@ export default function Info() {
                             </Paper>
                         </Grid>
                     </Grid>
-
-                    <Stack spacing={2} direction="row" alignItems="center" justifyContent="space-between" sx={{ mt: 4 }}>
-                        <Chart options={apexChart24.options} series={apexChart24.series} type="area" height={350} width={500} />
-                        <Chart options={apexChart7.options} series={apexChart7.series} type="area" height={350} width={500} />
-                        <Chart options={apexChart30.options} series={apexChart30.series} type="area" height={350} width={500} />
-                    </Stack>
                 </Container>
             </Box>
         </>
