b2b2d136ce438f659fc0f940b83836d64605946b
xenos#1337 <66328734+xenos1337@users.noreply.github.com>
2022-09-18 08:17:13 +0200

    Update @me.ts

--- pages/api/v1/users/@me.ts
+++ pages/api/v1/users/@me.ts
@@ -15,10 +15,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 if (!req.headers.authorization) return res.status(400).json({ success: false, message: "No authorization header provided" });
                 const token = req.headers.authorization as string;
 
-                // rate limits
                 limiter.check(res, 60, "CACHE_TOKEN");
 
-                // send a request to discord.com/api/users/@me
                 await axios.get("https://discord.com/api/users/@me", {
                     headers: {
                         Authorization: `${token}`,
@@ -27,10 +25,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                     proxy: false,
                     httpsAgent: new HttpsProxyAgent(`https://${process.env.PROXY_USERNAME}:${process.env.PROXY_PASSWORD}@zproxy.lum-superproxy.io:22225`)
                 }).then(async (response) => {
-                    // if the request was successful, return the user object
                     return res.status(200).json(response.data);
                 }).catch((error) => {
-                    // if the request failed, return the error
                     return res.status(400).json({ success: false, message: error.message });
                 });
             } catch (e) {
