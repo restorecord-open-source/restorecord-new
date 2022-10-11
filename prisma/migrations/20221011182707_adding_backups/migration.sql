0472a61d846e6fd7d8f0c4b9524bb474ba4cd519
xenos#1337 <66328734+xenos1337@users.noreply.github.com>
2022-10-11 20:44:59 +0200

    small fix

--- prisma/migrations/20221011182707_adding_backups/migration.sql
+++ prisma/migrations/20221011182707_adding_backups/migration.sql
@@ -3,7 +3,7 @@
 
   - You are about to drop the column `serverId` on the `backups` table. All the data in the column will be lost.
   - A unique constraint covering the columns `[userId,guildId]` on the table `guildMembers` will be added. If there are existing duplicate values, this will fail.
-  - Made the column `roleId` on table `channelpermissions` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `roleId` on table `channelPermissions` required. This step will fail if there are existing NULL values in that column.
 
 */
 -- DropForeignKey
@@ -13,7 +13,7 @@ ALTER TABLE `backups` DROP FOREIGN KEY `backups_serverId_fkey`;
 ALTER TABLE `backups` DROP COLUMN `serverId`;
 
 -- AlterTable
-ALTER TABLE `channelpermissions` MODIFY `roleId` BIGINT UNSIGNED NOT NULL;
+ALTER TABLE `channelPermissions` MODIFY `roleId` BIGINT UNSIGNED NOT NULL;
 
 -- AlterTable
 ALTER TABLE `roles` ADD COLUMN `botId` BIGINT NULL;
