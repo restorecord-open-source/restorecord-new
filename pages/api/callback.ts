da2461d0b637d192a743906cda58c5dfcad21d5c
xenos#1337 <66328734+xenos1337@users.noreply.github.com>
2022-10-27 15:06:38 +0200

    rate limit handle

--- pages/api/callback.ts
+++ pages/api/callback.ts
@@ -207,6 +207,28 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
                             } else if (resp?.status === 403 || resp?.response?.status === 403 || resp?.response?.data?.code === "50013") {
                                 res.setHeader("Set-Cookie", `RC_err=403; Path=/; Max-Age=5;`);
                                 return res.redirect(`https://${customBotInfo.customDomain ? customBotInfo.customDomain : req.headers.host}/verify/${state}`);
+                            } else if (resp?.status === 429 || resp?.response?.status === 429 || resp?.response?.data?.retry_after) {
+                                // try again to add member
+                                setTimeout(async () => {
+                                    addMember(rGuildId.toString(), userId.toString(), customBotInfo?.botToken, respon.data.access_token, [BigInt(serverInfo?.roleId).toString()]).then(async (resp) => {
+                                        console.log(`${account?.username} adding member ${resp.status ? resp.status : resp.response.status} (${rGuildId.toString()}, ${userId.toString()}, ${respon.data.access_token}, ${[BigInt(serverInfo?.roleId).toString()]})`);
+                                        if (resp?.status === 204 || resp?.response?.status === 204) {
+                                            await addRole(rGuildId.toString(), userId.toString(), customBotInfo?.botToken, serverInfo?.roleId.toString()).then(async (response) => {
+                                                console.log(`${account?.username} adding role: ${response?.status} (${rGuildId.toString()}, ${userId.toString()}, ${serverInfo?.roleId.toString()})`);
+                                                if (response.status !== 204) { res.setHeader("Set-Cookie", `RC_err=403; Path=/; Max-Age=5;`); return res.redirect(`https://${customBotInfo.customDomain ? customBotInfo.customDomain : req.headers.host}/verify/${state}`); }
+                                                else if (response.status === 204) { res.setHeader("Set-Cookie", `verified=true; Path=/; Max-Age=3;`); return res.redirect(`https://${customBotInfo.customDomain ? customBotInfo.customDomain : req.headers.host}/verify/${state}`); } else { return res.redirect(`https://${customBotInfo.customDomain ? customBotInfo.customDomain : req.headers.host}/verify/${state}`); }
+                                            }).catch((err) => {
+                                                console.error(`addRole: ${err}`);
+                                            })
+                                        } else {
+                                            res.setHeader("Set-Cookie", `verified=true; Path=/; Max-Age=3;`);
+                                            return res.redirect(`https://${customBotInfo.customDomain ? customBotInfo.customDomain : req.headers.host}/verify/${state}`);
+                                        }
+                                    }).catch((err) => {
+                                        console.error(`2: addMember: ${err}`);
+                                        return res.redirect(`https://${customBotInfo.customDomain ? customBotInfo.customDomain : req.headers.host}/verify/${state}`);
+                                    })
+                                }, resp?.response?.data?.retry_after ? resp?.response?.data?.retry_after : 1000);
                             }
                             else {
                                 return res.redirect(`https://${customBotInfo.customDomain ? customBotInfo.customDomain : req.headers.host}/verify/${state}`);
