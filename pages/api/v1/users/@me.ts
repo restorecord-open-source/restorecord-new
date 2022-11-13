f76a717b6222859b014dd98e3bb2b072b4c0582c
xenos#1337 <66328734+xenos1337@users.noreply.github.com>
2022-11-13 14:26:51 +0100

    Update @me.ts

--- pages/api/v1/users/@me.ts
+++ pages/api/v1/users/@me.ts
@@ -9,32 +9,26 @@ const limiter = rateLimit({
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     return new Promise(async (resolve, reject) => {
-        switch (req.method) {
-        case "GET":
-            try {
-                if (!req.headers.authorization) return res.status(400).json({ success: false, message: "No authorization header provided" });
-                const token = req.headers.authorization as string;
+        try {
+            if (!req.headers.authorization) return res.status(400).json({ success: false, message: "No authorization header provided" });
+            const token = req.headers.authorization as string;
 
-                limiter.check(res, 60, "CACHE_TOKEN");
+            limiter.check(res, 60, "CACHE_TOKEN");
 
-                await axios.get("https://discord.com/api/users/@me", {
-                    headers: {
-                        Authorization: `${token}`,
-                        "Cache-Control": "max-age=604800",
-                    },
-                    validateStatus: () => true,
-                    proxy: false,
-                    httpsAgent: new HttpsProxyAgent(`https://${process.env.PROXY_USERNAME}:${process.env.PROXY_PASSWORD}@zproxy.lum-superproxy.io:22225`)
-                }).then(async (response) => {
-                    return res.status(200).json(response.data);
-                }).catch((error) => {
-                    return res.status(400).json({ success: false, message: error.message });
-                });
-            } catch (e) {
-                return res.status(500).json({ success: false, message: "Internal server error" });
-            }
-        default:
-            return res.status(405).json({ success: false, message: "Method not allowed" });
+            await axios.get("https://discord.com/api/users/@me", {
+                headers: {
+                    Authorization: `${token}`,
+                },
+                validateStatus: () => true,
+                proxy: false,
+                httpsAgent: new HttpsProxyAgent(`https://${process.env.PROXY_USERNAME}:${process.env.PROXY_PASSWORD}@zproxy.lum-superproxy.io:22225`)
+            }).then(async (response) => {
+                return res.status(200).json(response.data);
+            }).catch((error) => {
+                return res.status(400).json({ success: false, message: error.message });
+            });
+        } catch (e) {
+            return res.status(500).json({ success: false, message: "Internal server error" });
         }
     });
 }
