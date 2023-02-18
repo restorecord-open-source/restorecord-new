c897125a22fbfe19b592bfccce5d5a43fa91f4e3
xenos#1337 <66328734+xenos1337@users.noreply.github.com>
2023-02-18 19:05:08 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,24 @@
 git pull
+
 yarn
+
+BUILD_DIR=temp
+
 yarn build
-pm2 restart all
+
+if [ ! -d "temp" ]; then
+  echo '\033[31m temp Directory not exists!\033[0m'  
+  exit 1;
+fi
+
+rm -rf .next
+
+if [ ! -d ".next" ]; then
+  mkdir .next
+fi
+
+mv temp/* .next
+
+rm -rf temp
+
+pm2 reload app --update-env
