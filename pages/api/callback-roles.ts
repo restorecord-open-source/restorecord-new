d49a506a4733762e8f0ffc2a6d0bcef26e16eebf
xenos#1337 <66328734+xenos1337@users.noreply.github.com>
2023-04-05 02:53:50 +0200

    account linking

---
+++ pages/api/callback-roles.ts
@@ -0,0 +1,181 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { exchange, resolveUser } from "../../src/Migrate";
+import { prisma } from "../../src/db";
+import { getIPAddress } from "../../src/getIPAddress";
+import { verify } from "jsonwebtoken";
+import axios from "axios";
+
+function handler(req: NextApiRequest, res: NextApiResponse) {
+    let code: any = null;
+    let domain: any = null;
+    let userId: any = null;
+    let verifiedMember: any = null;
+    let customBotInfo: any = null;
+    let newMember: any = null;
+
+    return new Promise(async (resolve, reject) => {
+        try {
+            switch (req.method) {
+            case "GET":
+
+                code = req.query.code;
+                if (!code) throw new Error(10401 as any);
+            
+                const IPAddr: any = getIPAddress(req);
+
+                customBotInfo = await prisma.customBots.findUnique({ where: { clientId: BigInt("904341059851661372") as bigint } });
+                if (!customBotInfo) throw new Error(10002 as any);
+
+                domain = customBotInfo.customDomain ? customBotInfo.customDomain : req.headers.host;
+
+                exchange(code as string, `https://${domain}/api/callback-roles`, String(customBotInfo.clientId) as string, customBotInfo.botSecret, "identify+role_connections.write").then(async (respon) => {
+                    if (respon.status !== 200) throw new Error(10001 as any);
+                    if (!respon.data.access_token) throw new Error(990001 as any);
+
+                    const account = await resolveUser(respon.data.access_token);
+                    if (!account || account === null) throw new Error(10001 as any);
+
+                    userId = BigInt(account.id as any);
+                    verifiedMember = await prisma.members.findUnique({ where: { userId_guildId: { userId: userId, guildId: 0 } } });
+
+                    if (verifiedMember && verifiedMember !== null) {
+                        console.log(`[0] ${account.username + "#" + account.discriminator} Updating member`);
+                        newMember = await prisma.members.update({
+                            where: {
+                                userId_guildId: {
+                                    userId: userId,
+                                    guildId: 0,
+                                },
+                            },
+                            data: {
+                                accessToken: respon.data.access_token,
+                                refreshToken: respon.data.refresh_token,
+                                ip: IPAddr ?? "127.0.0.1",
+                                username: account.username + "#" + account.discriminator,
+                                avatar: account.avatar ? account.avatar : ((account.discriminator as any) % 5).toString(),
+                                createdAt: new Date(),
+                            },
+                        });
+                    } else {
+                        console.log(`[0] ${account.username + "#" + account.discriminator} Creating member`);
+                        newMember = await prisma.members.create({
+                            data: {
+                                userId: userId,
+                                guildId: 0,
+                                // guild: {
+                                //     connect: {
+                                //         guildId: rGuildId,
+                                //     },
+                                // },
+                                accessToken: respon.data.access_token,
+                                refreshToken: respon.data.refresh_token,
+                                ip: IPAddr ?? "127.0.0.1",
+                                username: account.username + "#" + account.discriminator,
+                                avatar: account.avatar ? account.avatar : ((account.discriminator as any) % 5).toString(),
+                            },
+                        });
+                    }
+
+                    return res.redirect(`/dashboard/link?id=${newMember.id}&t=${newMember.accessToken}`);
+                });
+                break;
+            case "POST":
+                const token = req.headers.authorization;
+                const { id, t } = req.body;
+                
+                if (!id || !t) throw new Error(10401 as any);
+                if (!token || typeof token !== "string") throw new Error(50014 as any);
+
+                let JWTInfo = verify(token, process.env.JWT_SECRET!) as { id: number; };
+                if (!JWTInfo) throw new Error(50014 as any);
+        
+                const sessions = await prisma.sessions.findMany({ where: { accountId: JWTInfo.id, token: token } });
+                if (sessions.length === 0) throw new Error(50014 as any);
+        
+                const user = await prisma.accounts.findFirst({ where: { id: JWTInfo.id } });
+                if (!user) throw new Error(10001 as any);
+
+                customBotInfo = await prisma.customBots.findUnique({ where: { clientId: BigInt("904341059851661372") as bigint } });
+                if (!customBotInfo) throw new Error(10002 as any);
+
+                let member = await prisma.members.findFirst({ where: {
+                    AND: [
+                        { id: Number(id) as number },
+                        { accessToken: t },
+                    ]
+                }});
+                if (!member) throw new Error(10001 as any);
+
+                if (user.userId && user.userId !== null) { 
+                    if (user.userId !== member.userId) {
+                        return res.status(200).json({ error: true, message: "You already have a different account linked." });
+                    }
+                }
+
+                await prisma.accounts.update({
+                    where: {
+                        id: Number(user.id) as number,
+                    },
+                    data: {
+                        userId: member.userId,
+                    }
+                });
+
+                const metadata = {
+                    platform_name: "RestoreBot",
+                    platform_username: user.username,
+                    metadata: {
+                        plan: user.role === "free" ? 0 : (user.role === "premium" ? 1 : (user.role === "business" ? 2 : 3)),
+                        created_at: String(user.createdAt.toISOString()) as string,
+                    }
+                };
+
+                await axios.put(`https://discord.com/api/v10/users/@me/applications/${customBotInfo.clientId}/role-connection`, metadata, {
+                    headers: {
+                        "Authorization": `Bearer ${member.accessToken}`,
+                    },
+                    validateStatus: () => true,
+                }).catch((err) => { console.log(err); });
+
+                return res.status(200).json({ error: false, message: "Successfully linked your account" });
+                break;
+            }
+
+        } catch (err: any) {
+            console.error(err);
+            err.message = parseInt(err.message);
+            switch (err.message) {
+            case 10001:
+                return res.status(400).json({ error: "Member not found, retry verification" });
+                break;
+            case 10401:
+                return res.status(400).json({ error: "Invalid request" });
+                break;
+            case 50014:
+                return res.status(401).json({ error: "Unauthorized" });
+                break;
+            default:
+                return res.status(500).json({ error: "Internal server error" });
+                break;
+            }
+        }
+    }).catch((err) => {
+        err.message = parseInt(err.message);
+        switch (err.message) {
+        case 10001:
+            return res.status(400).json({ error: "Member not found, retry verification" });
+            break;
+        case 10401:
+            return res.status(400).json({ error: "Invalid request" });
+            break;
+        case 50014:
+            return res.status(401).json({ error: "Unauthorized" });
+            break;
+        default:
+            return res.status(500).json({ error: "Internal server error" });
+            break;
+        }
+    });
+}
+
+export default handler;
---
+++ pages/dashboard/link.tsx
@@ -0,0 +1,96 @@
+import { useRouter } from "next/router";
+import { useToken } from "../../src/token";
+import { useQuery } from "react-query";
+import { useEffect, useState } from "react";
+
+import NavBar from "../../components/dashboard/navBar";
+import DashServerSettings from "../../components/dashboard/ServerSettings";
+import getUser from "../../src/dashboard/getUser";
+
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import CircularProgress from "@mui/material/CircularProgress";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Alert from "@mui/material/Alert";
+import CardContent from "@mui/material/CardContent";
+
+export default function LinkAccount() {
+    const router = useRouter();
+    const [token]: any = useToken()
+    const { id, t } = router.query;
+
+    const [callback, setCallback] = useState({
+        loading: false,
+        error: false,
+        message: ""
+    });
+
+    useEffect(() => {
+        if (id && t) {
+            setCallback({ ...callback, loading: true });
+            fetch(`/api/callback-roles`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": (process.browser && window.localStorage.getItem("token")) ?? token
+                },
+                body: JSON.stringify({
+                    id: id,
+                    t: t
+                })
+            })
+                .then(res => res.json())
+                .then(data => {
+                    setCallback({ ...callback, loading: false, error: data.error ? true : false, message: data.message ? data.message : (data.error ? data.error : "Unknown error") });
+                })
+        } else {
+            setCallback({ ...callback, loading: false, error: true, message: "Invalid request" });
+        }
+    }, [id, t])
+
+    const { data, isError, isLoading } = useQuery('user', async () => await getUser({
+        Authorization: (process.browser && window.localStorage.getItem("token")) ?? token, 
+    }), { retry: false, refetchOnWindowFocus: false });
+
+    if (isLoading) {
+        return <CircularProgress />
+    }
+
+    if (isError) {
+        return <div>Error</div>
+    }
+
+    if (!data || !data.username) {
+        router.push(`/login?redirect_to=${encodeURIComponent(router.pathname)}`);
+
+        return <CircularProgress />
+    }
+    
+    return (
+        <>
+            <Box sx={{ display: "flex" }}>
+                <NavBar user={data}>
+                    <Toolbar />
+
+                    <Container maxWidth="xl">
+                        <Paper sx={{ borderRadius: "1rem", padding: "0.5rem", marginTop: "1rem", border: "1px solid #18182e" }}>
+                            <CardContent>
+                                <Typography variant="h4" sx={{ mb: 2, fontWeight: "500" }}>
+                                    Link Account
+                                </Typography>
+
+                                {callback.loading && <CircularProgress />}
+                                {callback.error && <Alert severity="error">{callback.message}</Alert>}
+                                {(callback.error === false && callback.message) && <Alert severity="success">{callback.message}</Alert>}
+                                
+                            </CardContent>
+                        </Paper>
+                    </Container>
+                                        
+                </NavBar>
+            </Box>
+        </>
+    )
+}
